e2e9779110b3d018129d3278911cf3b610ea83f9
jackqiu <2795275206@qq.com>
2023-07-23 18:16:40 +0800

    初始化项目

---
+++ mymall-coupon/src/main/java/com/jakcqiu/mymall/coupon/MymallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.jakcqiu.mymall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MymallCouponApplication.class, args);
+    }
+
+}
---
+++ mymall-product/src/test/java/com/jakcqiu/mymall/product/MymallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakcqiu.mymall.product;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymallProductApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
