40dc5a6a88e864a06ae14363cf4d741a0d96b0b1
jackqiu <2795275206@qq.com>
2023-07-24 11:09:55 +0800

    SpringCloud和SpringBoot的版本要对应

--- mymall-product/src/test/java/com/jackqiu/mymall/product/MymallProductApplicationTests.java
+++ mymall-product/src/test/java/com/jackqiu/mymall/product/MymallProductApplicationTests.java
@@ -1,7 +1,7 @@
 package com.jackqiu.mymall.product;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import com.jackqiu.mymall.product.entity.BrandEntity;
 import com.jackqiu.mymall.product.service.BrandService;
 import org.junit.runner.RunWith;
@@ -13,13 +13,13 @@ import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class MymallProductApplicationTests {
+public class MymallProductApplicationTests {
 
     @Autowired
     BrandService brandService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
         /*BrandEntity brandEntity = new BrandEntity();
         brandEntity.setName("qwe");
         brandService.save(brandEntity);
@@ -30,8 +30,8 @@ class MymallProductApplicationTests {
         brandEntity.setDescript("666");
         brandService.updateById(brandEntity);*/
 
-        List<BrandEntity> brand_id = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1));
-        brand_id.forEach(System.out::println);
+        /*List<BrandEntity> brand_id = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1));
+        brand_id.forEach(System.out::println);*/
     }
 
 }
